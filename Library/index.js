8a3e85165ec9a13bf1ecb787e0c4114603693093
lukkasz323 <39344794+lukkasz323@users.noreply.github.com>
2023-07-07 12:24:17 +0200

    Make Library functional

--- Library/index.js
+++ Library/index.js
@@ -1,5 +1,27 @@
-function addBookButton_onclick(event) {
-    // Toggle "Add book" menu
+class Book {
+    title;
+    author;
+    constructor(title, author) {
+        this.title = title;
+        this.author = author;
+    }
+}
+function addBook_menuButton_event_onclick(event) {
+    addBook_toggleMenu();
+}
+function addBook_submitButton_event_onclick(event) {
+    const title = document.getElementById("add-book-title");
+    const author = document.getElementById("add-book-author");
+    if (!title.value || !author.value) {
+        return;
+    }
+    globalThis.books.push(new Book(title.value, author.value));
+    title.value = "";
+    author.value = "";
+    addBook_toggleMenu();
+    reloadDOM();
+}
+function addBook_toggleMenu() {
     const menu = document.getElementById("add-book");
     const display = "inline";
     if (menu.style.display !== display) {
@@ -9,41 +31,41 @@ function addBookButton_onclick(event) {
         menu.style.display = "none";
     }
 }
-class Book {
-    title;
-    author;
-    constructor(title, author) {
-        this.title = title;
-        this.author = author;
+function generateLibrary() {
+    const library = document.createElement('table');
+    // Head
+    {
+        const head = library.createTHead();
+        const row = head.insertRow();
+        const nameCell = row.insertCell();
+        const authorCell = row.insertCell();
+        nameCell.innerText = "Title";
+        authorCell.innerText = "Author";
     }
+    // Body
+    {
+        const body = library.createTBody();
+        for (const book of globalThis.books) {
+            const row = body.insertRow();
+            const titleCell = row.insertCell();
+            const authorCell = row.insertCell();
+            titleCell.innerText = book.title;
+            authorCell.innerText = book.author;
+        }
+    }
+    return library;
 }
-// Main
-{
-    const books = [];
+function reloadDOM() {
+    const library = document.getElementById("library");
+    const newLibrary = generateLibrary();
+    newLibrary.id = library.id;
+    library.replaceWith(newLibrary);
+}
+function main() {
+    globalThis.books = [];
+    globalThis.isAddBookMenuOpen = false;
     books.push(new Book("BookName1", "BookAuthor1"));
     books.push(new Book("BookName2", "BookAuthor1"));
-    // Append books to the library table
-    {
-        const library = document.getElementById("library");
-        // Head
-        {
-            const head = library.createTHead();
-            const row = head.insertRow();
-            const nameCell = row.insertCell();
-            const authorCell = row.insertCell();
-            nameCell.innerText = "Title";
-            authorCell.innerText = "Author";
-        }
-        // Body
-        {
-            const body = library.createTBody();
-            for (const book of books) {
-                const row = body.insertRow();
-                const titleCell = row.insertCell();
-                const authorCell = row.insertCell();
-                titleCell.innerText = book.title;
-                authorCell.innerText = book.author;
-            }
-        }
-    }
+    reloadDOM();
 }
+main();
--- Library/index.ts
+++ Library/index.ts
@@ -1,5 +1,31 @@
-function addBookButton_onclick(event: PointerEvent) {
-    // Toggle "Add book" menu
+declare var books: Book[];
+declare var isAddBookMenuOpen: boolean;
+
+class Book {
+    constructor(public title: string, public author: string) {}
+}
+
+function addBook_menuButton_event_onclick(event: PointerEvent) {
+    addBook_toggleMenu();
+}
+
+function addBook_submitButton_event_onclick(event: PointerEvent) {
+    const title = <HTMLInputElement>document.getElementById("add-book-title");
+    const author = <HTMLInputElement>document.getElementById("add-book-author");
+
+    if (!title.value || !author.value) {
+        return;
+    }
+
+    globalThis.books.push(new Book(title.value, author.value));
+    title.value = "";
+    author.value = "";
+
+    addBook_toggleMenu();
+    reloadDOM();
+}
+
+function addBook_toggleMenu() {
     const menu = document.getElementById("add-book");
     const display = "inline";
 
@@ -11,42 +37,53 @@ function addBookButton_onclick(event: PointerEvent) {
     }
 }
 
-class Book {
-    constructor(public title: string, public author: string) {}
+function generateLibrary(): HTMLTableElement {
+    const library: HTMLTableElement = document.createElement('table');
+
+    // Head
+    {
+        const head: HTMLTableSectionElement = library.createTHead();
+
+        const row: HTMLTableRowElement = head.insertRow();
+        const nameCell: HTMLTableCellElement = row.insertCell();
+        const authorCell: HTMLTableCellElement = row.insertCell();
+        nameCell.innerText = "Title";
+        authorCell.innerText = "Author";
+    }
+
+    // Body
+    {
+        const body: HTMLTableSectionElement = library.createTBody();
+
+        for (const book of globalThis.books) {
+            const row: HTMLTableRowElement = body.insertRow();
+            const titleCell: HTMLTableCellElement = row.insertCell();
+            const authorCell: HTMLTableCellElement = row.insertCell();
+            titleCell.innerText = book.title;
+            authorCell.innerText = book.author;
+        }
+    }
+
+    return library;
 }
 
-// Main
-{
-    const books: Book[] = [];
+
+
+function reloadDOM() {
+    const library = <HTMLTableElement>document.getElementById("library");
+    const newLibrary = generateLibrary();
+    newLibrary.id = library.id;
+    library.replaceWith(newLibrary);
+}
+
+function main() {
+    globalThis.books = [];
+    globalThis.isAddBookMenuOpen = false;
+
     books.push(new Book("BookName1", "BookAuthor1"));
     books.push(new Book("BookName2", "BookAuthor1"));
     
-    // Append books to the library table
-    {
-        const library = <HTMLTableElement>document.getElementById("library");
-        
-        // Head
-        {
-            const head: HTMLTableSectionElement = library.createTHead();
-
-            const row: HTMLTableRowElement = head.insertRow();
-            const nameCell: HTMLTableCellElement = row.insertCell();
-            const authorCell: HTMLTableCellElement = row.insertCell();
-            nameCell.innerText = "Title";
-            authorCell.innerText = "Author";
-        }
-
-        // Body
-        {
-            const body: HTMLTableSectionElement = library.createTBody();
-    
-            for (const book of books) {
-                const row: HTMLTableRowElement = body.insertRow();
-                const titleCell: HTMLTableCellElement = row.insertCell();
-                const authorCell: HTMLTableCellElement = row.insertCell();
-                titleCell.innerText = book.title;
-                authorCell.innerText = book.author;
-            }
-        }
-    }
+    reloadDOM();
 }
+
+main();
